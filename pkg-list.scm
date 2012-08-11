3e51cc90715d2f14cf7df263e79cbde38e5ca003
Ian Price <ianprice90@googlemail.com>
2012-08-11 16:20:06 +0100

    Add pkg-list.scm

---
+++ pkg-list.scm
@@ -0,0 +1,6 @@
+(package (bloom-filter (0 1))
+  (synopsis "Bloom Filter in Scheme")
+  (description
+   "A quick and dirty bloom filter to show off guile's bitvectors.")
+  (homepage "http://github.com/ijp/bloom-filter")
+  (libraries "bloom-filter.scm"))
